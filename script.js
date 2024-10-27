c9290890ed564ae6a50cfc006f2c2381d53c5f0a
Nicholas Lucky <148924180+Nicholas-Lucky@users.noreply.github.com>
2024-10-26 20:09:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function startMovingMouseGifOnLoad() {
   const mouse = document.getElementById("running-mouse");
   mouse.style.display = "block"; // Show the mouse to begin
-  
-  const STARTING_POSITION = -300; // Starting position off-screen
-  let position = STARTING_POSITION; // Current position off-screen
+
   const screenWidth = window.innerWidth; // Get screen width
+  const STARTING_POSITION = -1 * (screenWidth / 2); // Starting position off-screen
+  let position = STARTING_POSITION; // Current position off-screen
 
   function moveMouse() {
     position += 5; // Adjust speed (higher value means faster)
@@ -20,7 +20,7 @@ function startMovingMouseGifOnLoad() {
       // -------------------------------------------------------------------------------
       position = STARTING_POSITION; // Reset position to the starting position
       mouse.style.left = position + "px";
-      
+
       mouse.style.display = "block"; // Show the mouse again after it is resetted
       requestAnimationFrame(moveMouse);
       // -------------------------------------------------------------------------------

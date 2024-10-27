120b1d19afeb3a094caf164e026fe72276a97cc9
Nicholas Lucky <148924180+Nicholas-Lucky@users.noreply.github.com>
2024-10-26 19:52:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-window.onload = function () {
+function startMovingMouseGifOnLoad() {
   const mouse = document.getElementById("running-mouse");
-  let position = -150; // Starting position off-screen
+  mouse.style.display = "block"; // Show the mouse to begin
+  
+  const STARTING_POSITION = -300; // Starting position off-screen
+  let position = STARTING_POSITION; // Current position off-screen
   const screenWidth = window.innerWidth; // Get screen width
 
   function moveMouse() {
@@ -8,12 +11,21 @@ window.onload = function () {
     mouse.style.left = position + "px";
 
     if (position < screenWidth) {
+      console.log("HI")
       requestAnimationFrame(moveMouse); // Continue moving
     } else {
       mouse.style.display = "none"; // Hide the mouse when it reaches the end
+
+      // vv COMMENT OUT/DELETE IF WE DONT WANT THE ANIMATION TO LOOP
+      // -------------------------------------------------------------------------------
+      position = STARTING_POSITION; // Reset position to the starting position
+      mouse.style.left = position + "px";
+      
+      mouse.style.display = "block"; // Show the mouse again after it is resetted
+      requestAnimationFrame(moveMouse);
+      // -------------------------------------------------------------------------------
     }
   }
 
   moveMouse(); // Start the mouse movement
-};
-
+}

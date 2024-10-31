f17b942c324d9a9e60c17c13c687b755568a761c
Nicholas Lucky <148924180+Nicholas-Lucky@users.noreply.github.com>
2024-10-30 21:02:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Function for the running mouse on card2.html
 function startMovingMouseGifOnLoad() {
   const mouse = document.getElementById("running-mouse");
   mouse.style.display = "block"; // Show the mouse to begin
@@ -11,7 +12,6 @@ function startMovingMouseGifOnLoad() {
     mouse.style.left = position + "px";
 
     if (position < screenWidth) {
-      console.log("HI")
       requestAnimationFrame(moveMouse); // Continue moving
     } else {
       mouse.style.display = "none"; // Hide the mouse when it reaches the end
@@ -28,4 +28,37 @@ function startMovingMouseGifOnLoad() {
   }
 
   moveMouse(); // Start the mouse movement
+}
+
+// Function for the float mouse on card3.html
+function startFloatingMouseGifOnLoad() {
+  const mouse = document.getElementById("floating-mouse");
+  mouse.style.display = "block"; // Show the mouse to begin
+
+  const screenHeight = window.innerHeight; // Get screen height
+  const STARTING_POSITION = -1 * (screenHeight / 2); // Starting position off-screen
+  let position = STARTING_POSITION; // Current position off-screen
+
+  function floatMouseDown() {
+    position += 2; // Adjust speed (higher value means faster)
+    mouse.style.top = position + "px";
+
+    if (position < screenHeight) {
+      console.log("HI")
+      requestAnimationFrame(floatMouseDown); // Continue moving
+    } else {
+      mouse.style.display = "none"; // Hide the mouse when it reaches the end
+
+      // vv COMMENT OUT/DELETE IF WE DONT WANT THE ANIMATION TO LOOP
+      // -------------------------------------------------------------------------------
+      position = STARTING_POSITION; // Reset position to the starting position
+      mouse.style.top = position + "px";
+
+      mouse.style.display = "block"; // Show the mouse again after it is resetted
+      requestAnimationFrame(floatMouseDown);
+      // -------------------------------------------------------------------------------
+    }
+  }
+
+  floatMouseDown(); // Start the mouse movement
 }
